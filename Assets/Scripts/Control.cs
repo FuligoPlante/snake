48dd76ad5cccc9f13ac462b6e7f0af1c09a1b162
Fuligo <3228429187@qq.com>
2024-04-03 23:23:49 +0800

    new file:   Assets/Animation/HeadDown.anim 	new file:   Assets/Animation/HeadDown.anim.meta 	new file:   Assets/Animation/HeadLeft.anim 	new file:   Assets/Animation/HeadLeft.anim.meta 	new file:   Assets/Animation/HeadRight.anim 	new file:   Assets/Animation/HeadRight.anim.meta 	new file:   Assets/Animation/HeadUp.anim 	new file:   Assets/Animation/HeadUp.anim.meta 	modified:   Assets/Animation/PlayerMove.controller 	new file:   Assets/Animation/Tail.meta 	new file:   Assets/Animation/Tail/Horizontal.anim 	new file:   Assets/Animation/Tail/Horizontal.anim.meta 	new file:   Assets/Animation/Tail/Tail.anim 	new file:   Assets/Animation/Tail/Tail.anim.meta 	new file:   Assets/Animation/Tail/TailControl.controller 	new file:   Assets/Animation/Tail/TailControl.controller.meta 	new file:   Assets/Animation/Tail/TailDown.anim 	new file:   Assets/Animation/Tail/TailDown.anim.meta 	new file:   Assets/Animation/Tail/TailLeft.anim 	new file:   Assets/Animation/Tail/TailLeft.anim.meta 	new file:   Assets/Animation/Tail/TailRight.anim 	new file:   Assets/Animation/Tail/TailRight.anim.meta 	new file:   Assets/Animation/Tail/TailUp.anim 	new file:   Assets/Animation/Tail/TailUp.anim.meta 	new file:   Assets/Animation/Tail/UpLeft.anim 	new file:   Assets/Animation/Tail/UpLeft.anim.meta 	new file:   Assets/Animation/Tail/Vertical.anim 	new file:   Assets/Animation/Tail/Vertical.anim.meta 	new file:   Assets/Resource/Map/sprite-21-2_0.asset 	new file:   Assets/Resource/Map/sprite-21-2_0.asset.meta 	new file:   Assets/Resource/Map/sprite-21-2_1.asset 	new file:   Assets/Resource/Map/sprite-21-2_1.asset.meta 	new file:   Assets/Resource/Map/stone.prefab 	new file:   Assets/Resource/Map/stone.prefab.meta 	new file:   Assets/Resource/Prefeb/Stone.prefab 	new file:   Assets/Resource/Prefeb/Stone.prefab.meta 	modified:   Assets/Resource/Prefeb/Tail.prefab 	modified:   Assets/Resource/graphics/1x/sprite-1-7.png.meta 	modified:   Assets/Resource/graphics/1x/sprite-21-2.png.meta 	modified:   Assets/Scenes/SampleScene.unity 	modified:   Assets/Scripts/Control.cs 	modified:   Assets/Scripts/TailControl.cs

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -12,6 +12,8 @@ public class Control : MonoBehaviour
     private List<GameObject> tail = new List<GameObject>();
     private Vector2 moveDirection;
     private Vector2 preDirection;
+    private Vector2 prepreDirection;
+    Animator animator;
     Vector2[,] dirMap = new Vector2[12, 2] { { Vector2.right, Vector2.right }, { Vector2.left, Vector2.left }, { Vector2.up, Vector2.up }, { Vector2.down, Vector2.down },
                                             { Vector2.right, Vector2.down }, { Vector2.up, Vector2.left }, { Vector2.left, Vector2.down }, { Vector2.up, Vector2.right },
                                             { Vector2.right, Vector2.up }, { Vector2.down, Vector2.left }, { Vector2.left, Vector2.up }, { Vector2.down, Vector2.right }};
@@ -19,7 +21,10 @@ public class Control : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        animator = GetComponent<Animator>();
+        preDirection = Vector2.right;
+        prepreDirection = Vector2.right;
+        addTail();
     }
 
     // Update is called once per frame
@@ -55,37 +60,93 @@ public class Control : MonoBehaviour
         if (collision.tag == "food")
         {
             Destroy(collision.gameObject);
-            if (tail.Count == 0)
-            {
-                tail.Add(Instantiate(tailPrefab, transform.position - new Vector3(preDirection.x,preDirection.y,0), Quaternion.identity));
-                tail.First().GetComponent<TailControl>().setDirection(preDirection);
-            }
-            else
-            {
-                GameObject tmp = tail.Last();
-                tail.Add(Instantiate(tailPrefab, tail[tail.Count - 1].GetComponent<TailControl>().calNewTail(), Quaternion.identity));
-                tmp.GetComponent<TailControl>().tailNext = tail.Last();
-            }
+            addTail();
         }
         else if (collision.tag == "tail")
         {
             Destroy(gameObject);
         }
     }
+    private void addTail()
+    {
+        if (tail.Count == 0)
+        {
+            tail.Add(Instantiate(tailPrefab, transform.position - new Vector3(preDirection.x, preDirection.y, 0), Quaternion.identity));
+            tail.First().GetComponent<TailControl>().setDirection(prepreDirection);
+            tail.First().GetComponent<TailControl>().setNextDirection(preDirection);
+            tail.First().GetComponent<TailControl>().IsTail(true);
+            tail.First().GetComponent<TailControl>().setAnimator();
+        }
+        else
+        {
+            GameObject tmp = tail.Last();
+            tail.Add(Instantiate(tailPrefab, tail[tail.Count - 1].GetComponent<TailControl>().calNewTail(), Quaternion.identity));
+            tmp.GetComponent<TailControl>().tailNext = tail.Last();
+            tail.Last().GetComponent<TailControl>().IsTail(true);
+            tmp.GetComponent<TailControl>().IsTail(false);
+            tmp.GetComponent<TailControl>().setAnimator();
+            tmp.GetComponent<TailControl>().setTailNextDirection();
+        }
+    }
     private void SnakeMove(Vector2 direction)
     {
+        setAnimator(direction);
         if (tail.Count > 0)
         {
             tail.First().GetComponent<TailControl>().setDirection(preDirection);
+            tail.First().GetComponent<TailControl>().setNextDirection(direction);
             tail.First().GetComponent<TailControl>().moveTail();
         }
+        prepreDirection = preDirection;
         preDirection = direction;
+        
         transform.Translate(direction * speed);
         
     }
 
     private bool IsCollided(Vector2 direction)
     {
+        RaycastHit2D hitObjects = Physics2D.Raycast(transform.position, direction,1.0f,LayerMask.GetMask("Obstacle"));
+        if(hitObjects.collider == null)
+        {
+            return false;
+        }
+        //Debug.Log(hitObjects.collider.tag);
+        if (hitObjects.collider.tag == "Stone")
+        {
+            return true;
+        }
         return false;
+    }
+    private void setAnimator(Vector2 direction)
+    {
+        if(direction == Vector2.up)
+        {
+            animator.SetBool("Up", true);
+            animator.SetBool("Down", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Right", false);
+        }
+        else if(direction == Vector2.down)
+        {
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", true);
+            animator.SetBool("Left", false);
+            animator.SetBool("Right", false);
+        }
+        else if(direction == Vector2.left)
+        {
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
+            animator.SetBool("Left", true);
+            animator.SetBool("Right", false);
+        }
+        else if(direction == Vector2.right)
+        {
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Right", true);
+        }
     }
 }
--- Assets/Scripts/TailControl.cs
+++ Assets/Scripts/TailControl.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class TailControl : MonoBehaviour
 {
     public GameObject tailNext;
+    public bool isTail = false;
+
     private float speed = 1.0f;
     private Vector2 direction;
     private Vector2 preDirection;
+    private Vector2 nextDirection;
+    
+    Animator animator;
+    
     // Start is called before the first frame update
     void Start()
     {
         direction = Vector2.zero;
         preDirection = Vector2.zero;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,9 +32,22 @@ public class TailControl : MonoBehaviour
     {
         direction = dir;
     }
+    public void setNextDirection(Vector2 dir)
+    {
+        nextDirection = dir;
+    }
     public Vector2 getDirection()
     {
         return direction;
+    }
+    public void setTailNextDirection()
+    {
+        if(tailNext != null)
+        {
+            tailNext.GetComponent<TailControl>().setDirection(preDirection);
+            tailNext.GetComponent<TailControl>().setNextDirection(direction);
+            tailNext.GetComponent<TailControl>().setAnimator();
+        }
     }
     public Vector3 calNewTail()
     {
@@ -35,12 +56,131 @@ public class TailControl : MonoBehaviour
     public void moveTail()
     {
         transform.Translate(direction * speed);
-        if(tailNext != null)
+        if (tailNext != null)
         {
             tailNext.GetComponent<TailControl>().setDirection(preDirection);
+            tailNext.GetComponent<TailControl>().setNextDirection(direction);
             tailNext.GetComponent<TailControl>().moveTail();
         }
+        setAnimator();
         preDirection = direction;
     }
+    
+    public void setAnimator()
+    {
+        //如果是尾巴，直接设置尾巴动画
+        if(isTail)
+        {
+            setTailAnimator();
+            return;
+        }
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+        if ((nextDirection == Vector2.left && direction == Vector2.left)||
+            (nextDirection == Vector2.right && direction == Vector2.right))
+        {
+            animator.SetBool("Horizontal", true);
+            animator.SetBool("Vertical", false);
+            animator.SetBool("IsTurn", false);
+        }
+        else if((nextDirection == Vector2.up && direction == Vector2.up) ||
+            (nextDirection == Vector2.down && direction == Vector2.down))
+        {
+            animator.SetBool("Vertical", true);
+            animator.SetBool("Horizontal", false);
+            animator.SetBool("IsTurn", false);
+        }
+        else
+        {
+            animator.SetBool("IsTurn", true);
+            animator.SetBool("Horizontal", false);
+            animator.SetBool("Vertical", false);
+            SpriteRenderer sr = GetComponent<SpriteRenderer>();
+            //通过判断方向来确定旋转角度
+            if (direction == Vector2.left && nextDirection == Vector2.up)
+            {
+                sr.flipX = true;
+                sr.flipY = true;
+            }
+            else if(direction == Vector2.left && nextDirection == Vector2.down)
+            {
+                sr.flipX = true;
+                sr.flipY = false;
+            }
+            else if(direction == Vector2.right && nextDirection == Vector2.up)
+            {
+                sr.flipX = false;
+                sr.flipY = true;
+            }
+            else if(direction == Vector2.right && nextDirection == Vector2.down)
+            {
+                sr.flipX = false;
+                sr.flipY = false;
+            }
+            else if(direction == Vector2.up && nextDirection == Vector2.left)
+            {
+                sr.flipX = false;
+                sr.flipY = false;
+            }
+            else if(direction == Vector2.up && nextDirection == Vector2.right)
+            {
+                sr.flipX = true;
+                sr.flipY = false;
+            }
+            else if(direction == Vector2.down && nextDirection == Vector2.left)
+            {
+                sr.flipX = false;
+                sr.flipY = true;
+            }
+            else if(direction == Vector2.down && nextDirection == Vector2.right)
+            {
+                sr.flipX = true;
+                sr.flipY = true;
+            }
 
+        }
+    }
+    public void setTailAnimator()
+    {
+        
+        if (nextDirection == Vector2.up)
+        {
+            animator.SetBool("Up", true);
+            animator.SetBool("Down", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Right", false);
+        }
+        else if (nextDirection == Vector2.down)
+        {
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", true);
+            animator.SetBool("Left", false);
+            animator.SetBool("Right", false);
+        }
+        else if (nextDirection == Vector2.left)
+        {
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
+            animator.SetBool("Left", true);
+            animator.SetBool("Right", false);
+        }
+        else if (nextDirection == Vector2.right)
+        {
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Right", true);
+        }
+    }
+    public void IsTail(bool inbool)
+    {
+        isTail = inbool;
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+        animator.SetBool("IsTail", isTail);
+    }
 }
